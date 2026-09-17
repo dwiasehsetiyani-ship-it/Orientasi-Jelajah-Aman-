acc499a423d430cc33e35d5fcc3b7b2a68ca1232
Dwi Asih Setiani <dwi.asih.setiani 24044@mhs.uingusdur.ac.id>
2026-09-17 21:02:14 +0700

    chore: restrukturisasi folder untuk Expo Router

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,21 +1,20 @@
 // app/index.tsx
 import { View } from "react-native";
 import { useState, useEffect } from "react";
-// Catatan: gunakan ../components/ jika file berada di dalam folder app/
-// yang sejajar dengan folder components/
-import WeatherCard from "../components/WeatherCard";
-import SearchBox from "../components/SearchBox";
-import RiwayatList from "../components/RiwayatList";
-import IndikatorAQI from "../components/IndikatorAQI";
+
+import WeatherCard from "../../components/WeatherCard";
+import SearchBox from "../../components/SearchBox";
+import RiwayatList from "../../components/RiwayatList";
+import IndikatorAQI from "../../components/IndikatorAQI";
 
 export default function HalamanUtama() {
   const [kotaAktif, setKotaAktif] = useState("Pekalongan");
-  const [riwayat, setRiwayat] = useState<string[]>(["Pekalongan"]);
+  const [riwayat, setRiwayat] = useState<string[]> (["Pekalongan"]);
 
-// Tambahkan useEffect untuk mencatat perubahan kota aktif
-useEffect(() => {
-  console.log("Kota aktif berubah menjadi:", kotaAktif);
-}, [kotaAktif]);
+  // Tambahkan useEffect untuk mencatat perubahan kota aktif
+  useEffect(() => {
+    console.log ("Kota aktif berubah menjadi:", kotaAktif);
+  }, [kotaAktif]);
 
 function handleCari(kota: string) {
   setKotaAktif(kota);
@@ -25,7 +24,7 @@ if (!riwayat.includes(kota)) {
 }
 
 return (
-  <View style={{ padding: 16, gap: 16 }}>
+  <View style={{ padding: 16, paddingTop :45 , gap: 16 }}>
     <SearchBox onCari={handleCari} />
     <WeatherCard kota={kotaAktif} suhu={29} tingkatAQI="BAIK" />
     <RiwayatList daftarKota={riwayat} />
